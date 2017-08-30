33671f1a717cef5fe8f140542324b41ea4b3b75a
Chris <cltsd27@gmail.com>
2017-08-29 22:49:34 -0400

    Strip @'s and #'s from tweets

--- data_helpers.py
+++ data_helpers.py
@@ -9,7 +9,9 @@ def clean_str(string):
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
     """
     string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
-    string = re.sub(r"^https?:\/\/.*[\r\n]*", " ", string)
+    string = re.sub(r"https?:\/\/.*[\r\n]*", " ", string)
+    string = re.sub(r"@\w+", " ", string)
+    string = re.sub(r"#\w+", " ", string)
     string = re.sub(r"\'s", " \'s", string)
     string = re.sub(r"\'ve", " \'ve", string)
     string = re.sub(r"n\'t", " n\'t", string)
